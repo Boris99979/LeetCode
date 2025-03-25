f40c8ec8a09be7952f02e716043de5fc4ab5e464
Boris Dovgal <81147196+Boris99979@users.noreply.github.com>
2025-03-25 03:16:00 +0300

    Time: 1 ms (99.93%), Space: 34.7 MB (64.97%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cs
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -1,21 +1,13 @@
 public class Solution {
     public bool IsPalindrome(int x) {
-        if (x < 0)
-        {
-            return false;
+        int reverse = 0, original = x;
+        while (original > 0)
+        {   
+            reverse = reverse * 10 + original % 10;
+            original /= 10;
         }
-        int reverse = 0;
-        int original = x;
-        while (x != 0)
-        {
-            int remainder = x % 10;
-            reverse = reverse * 10 + remainder;
-            x /= 10;
-        }
-        if (reverse == original)
-        {
-            return true;
-        } else return false;
 
+        return x == reverse;
+       
     }
 }
