9f10b386b24a419e04abad4eb7d2225ce114a4b5
Boris Dovgal <81147196+Boris99979@users.noreply.github.com>
2025-03-26 03:48:47 +0300

    Time: 3 ms (69.91%), Space: 50.4 MB (58.96%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cs
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -1,26 +1,25 @@
-//Just a test case for optimization, not my real answer
-
 public class Solution {
     public int RomanToInt(string s) {
-        int total = 0;
-        int len = s.Length;
+        Dictionary<char, int> romanNum = new Dictionary<char, int>
+        {
+        {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100},
+            {'D', 500}, {'M', 1000}
+        };
+        int number = 0;
+        
 
-        for (int i = 0; i < len; i++) {
-            int curr = GetValue(s[i]);
-            int next = (i + 1 < len) ? GetValue(s[i + 1]) : 0;
+        for(int i = 0; i < s.Length; i++)
+        {
+            
+            int curr = romanNum[s[i]];
+            int next = (i + 1 < s.Length) ? romanNum[s[i + 1]] : 0;
 
-            if (curr < next)
-                total -= curr;
-            else
-                total += curr;
-        }
+            number += (curr < next) ? -curr : curr;
+        
+        } 
+        return number;           
 
-        return total;
     }
-
-    private static int GetValue(char c) => c switch {
-        'I' => 1, 'V' => 5, 'X' => 10,
-        'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000,
-        _ => 0
-    };
+   
 }
