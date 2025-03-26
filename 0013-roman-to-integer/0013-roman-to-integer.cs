be711ff5bccea9d3cfc9094368b1a969de70d9ab
Boris Dovgal <81147196+Boris99979@users.noreply.github.com>
2025-03-26 03:39:54 +0300

    Time: 3 ms (69.91%), Space: 50.7 MB (29.24%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cs
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -1,22 +1,19 @@
 public class Solution {
     public int RomanToInt(string s) {
-        Dictionary<char, int> romanNum = new();
-        romanNum.Add('I', 1);
-        romanNum.Add('V', 5);
-        romanNum.Add('X', 10);
-        romanNum.Add('L', 50);
-        romanNum.Add('C', 100);
-        romanNum.Add('D', 500);
-        romanNum.Add('M', 1000);
-
+        Dictionary<char, int> romanNum = new Dictionary<char, int>
+        {
+        {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100},
+            {'D', 500}, {'M', 1000}
+        };
         int number = 0;
-        char[] romanChar = s.ToCharArray();
+        
 
-        for(int i = 0; i < romanChar.Length; i++)
+        for(int i = 0; i < s.Length; i++)
         {
             
-            int curr = romanNum[romanChar[i]];
-            int next = (i + 1 < romanChar.Length) ? romanNum[romanChar[i + 1]] : 0;
+            int curr = romanNum[s[i]];
+            int next = (i + 1 < s.Length) ? romanNum[s[i + 1]] : 0;
 
             number += (curr < next) ? -curr : curr;
         
